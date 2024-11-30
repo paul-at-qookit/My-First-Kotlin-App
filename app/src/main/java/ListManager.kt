156ef7d697c1d82493118ef694964cc698dee599
Paul Franklin <pfranklin138@gmail.com>
2024-11-30 09:46:33 -0500

    Added and polished documentation

--- app/src/main/java/ListManager.kt
+++ app/src/main/java/ListManager.kt
@@ -1,28 +1,29 @@
 class ListManager {
+    //maintains collection of to-do tasks and is managed by functions of ListManager which add tasks, print tasks, and mark tasks complete
     private val listOfTasks = mutableListOf<SingleListItem>()
 
-    //adds new item to listOfTasks and includes String argument as taskDescription
+    //adds a new task to the list with the given description
     fun addItem(description: String) {
         val newItem = SingleListItem(taskDescription = description, itemID = listOfTasks.size + 1)
         listOfTasks.add(newItem)
     }
-    //finds to-do item based on provided ID number and changes completion status to "true"
+    //marks a task as complete based on its ID.
     fun markItemAsComplete(id: Int) {
         val itemCompleted = listOfTasks.find { it.itemID == id }
         itemCompleted?.isComplete = true
     }
-    //prints to console a string representation of all items in list of tasks and their properties
+    //displays all tasks and their properties in the console
     fun showAllTasks() {
         listOfTasks.forEach { println(it) }
     }
 }
-
+//instances of this class constitute to-do task objects utilized by ListManager and main()
 data class SingleListItem(
     var isComplete: Boolean = false,
     val itemID: Int = 0,
     var taskDescription: String
 )
-
+//entrypoint to Kotlin program where tasks are created, set as complete, and displayed in a String
 fun main () {
     val app = ListManager()
     app.addItem("Clean my socks")
