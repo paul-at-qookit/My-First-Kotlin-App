6dcae5f69ca24312db9f6d7fea77c0962678ecec
Paul Franklin <pfranklin138@gmail.com>
2024-11-30 17:02:51 -0500

    Completed logic in ListManager.kt and activity_main.xml

--- app/src/main/java/MainActivity.kt
+++ app/src/main/java/MainActivity.kt
@@ -1,5 +1,9 @@
 package com.example.platformbasedcriticalthinkingthree
+import ListManager
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {  // Extend AppCompatActivity
@@ -7,4 +11,18 @@ class MainActivity : AppCompatActivity() {  // Extend AppCompatActivity
         super.onCreate(savedInstanceState)  // Call the parent class method
         setContentView(R.layout.activity_main)  // Link to the XML layout
     }
+
+    private val myManager = ListManager()
+
+    val buttonForTaskAdd = findViewById<Button>(R.id.buttonForTaskAdd)
+    val textFieldForTasks = findViewById<EditText>(R.id.textFieldForTasks)
+    val markTaskDoneButton = findViewById<Button>(R.id.markTaskDoneButton)
+    val textFieldForID = findViewById<EditText>(R.id.textFieldForID)
+    val spaceForTaskList = findViewById<TextView>(R.id.spaceForTaskList)
+
+
+
+
+
+
 }
