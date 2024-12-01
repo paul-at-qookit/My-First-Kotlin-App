422620bb376cbe4e32faf3a2fc34c9d6fd65cad8
Paul-At-Qookit <paul@qookit.ai>
2024-11-30 19:07:45 -0500

    inital commit 707pm

--- app/src/main/java/MainActivity.kt
+++ app/src/main/java/MainActivity.kt
@@ -1,21 +1,17 @@
 package com.example.platformbasedcriticalthinkingthree
-<<<<<<< HEAD
 import ListManager
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
-=======
-import android.os.Bundle
->>>>>>> 57b2bb7 (solved gradle issues so MainActivity and AndroidManifest can function properly)
 import androidx.appcompat.app.AppCompatActivity
+import com.example.paullearnskotlin.R
 
 class MainActivity : AppCompatActivity() {  // Extend AppCompatActivity
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)  // Call the parent class method
         setContentView(R.layout.activity_main)  // Link to the XML layout
     }
-<<<<<<< HEAD
 
     private val myManager = ListManager()
 
@@ -24,12 +20,3 @@ class MainActivity : AppCompatActivity() {  // Extend AppCompatActivity
     val markTaskDoneButton = findViewById<Button>(R.id.markTaskDoneButton)
     val textFieldForID = findViewById<EditText>(R.id.textFieldForID)
     val spaceForTaskList = findViewById<TextView>(R.id.spaceForTaskList)
-
-
-
-
-
-
-=======
->>>>>>> 57b2bb7 (solved gradle issues so MainActivity and AndroidManifest can function properly)
-}
